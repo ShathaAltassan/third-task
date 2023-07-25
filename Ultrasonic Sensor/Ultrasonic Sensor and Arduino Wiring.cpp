8d0519bcc68e09922fa878921093ead9b802eb11
Shatha Altassan <139460879+ShathaAltassan@users.noreply.github.com>
2023-07-25 20:32:48 +0300

    code
    code

---
+++ Ultrasonic Sensor/Ultrasonic Sensor and Arduino Wiring.cpp
@@ -0,0 +1,35 @@
+// C++ code
+//
+const int trigPin = 9;
+const int echoPin = 10;
+
+float duration, distance;
+
+
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("setup is complete");
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop()
+{
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  
+  digitalWrite(trigPin, LOW);
+  
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration*.0343)/2;
+  
+  Serial.println(distance);
+  delay(100);
+  
+ 
+}
